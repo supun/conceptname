e10f7132932fc5288b9e43218065567d843637f4
supun <supun921@gmail.com>
2013-05-06 11:39:16 +0530

    Sample module

---
+++ omod/src/main/java/org/openmrs/module/conceptname/web/controller/ConceptNameManageController.java
@@ -0,0 +1,37 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.conceptname.web.controller;
+
+import org.openmrs.Concept;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * The main controller.
+ */
+@Controller
+public class  ConceptNameManageController {
+	
+	@ModelAttribute	
+	  public Concept getConcept(@RequestParam(required=false, value="conceptId")Concept concept){
+		return concept;
+	}	
+	  @RequestMapping(value="/module/conceptname/viewConceptNames",method=RequestMethod.GET)	
+	  public void showForm(){
+		  
+	  }
+}
